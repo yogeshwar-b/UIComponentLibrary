de1ab1952556637defe8547e6f89775fd920cafd
yogeshwar-b <byogeshwarreddy@gmail.com>
2023-11-07 00:20:42 +0530

    Added dark mode.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
+import "./App.css";
+
 /**
  * Returns text Initial Project.
  */
 function App() {
+  /**
+   * @todo Handle the default theme with checkbox
+   */
+  document.body.classList.add("home-bg", "dark-home-bg");
   return (
-    <>
-      <div>Initial Project</div>
-    </>
+    <div>
+      <input
+        type="checkbox"
+        name="checkboxtheme"
+        id="checkboxtheme"
+        onChange={(e) => {
+          if (e.target.checked) {
+            document.body.classList.add("home-bg", "dark-home-bg");
+          } else {
+            document.body.classList.remove(...document.body.classList);
+            document.body.classList.add("home-bg");
+          }
+        }}
+      />
+      <div className="text dark-text">Dark Mode</div>
+    </div>
   );
 }
 
