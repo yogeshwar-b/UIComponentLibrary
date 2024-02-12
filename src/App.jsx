91c7760b396615e4779f0d41dd2ec40cfb276f96
Yogeshwar B <byogeshwarreddy@gmail.com>
2024-02-12 22:12:47 +0530

    Theme button added and animated.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,22 @@
-import "./App.css";
-import ToggleSwitch from "./components/toggleswitch";
+import './App.css';
+import ToggleSwitch from './components/toggleswitch';
+import ThemeButton from './components/ThemeButton/ThemeButton';
 /**
  * Returns text Initial Project.
  * @todo Handle the default theme with checkbox
  */
 function App() {
-  document.body.classList.add("home-bg", "dark-home-bg");
+  document.body.classList.add('home-bg', 'dark-home-bg');
   return (
-    <div>
-      <input
-        type="checkbox"
-        name="checkboxtheme"
-        id="checkboxtheme"
-        onChange={(e) => {
-          if (e.target.checked) {
-            document.body.classList.add("home-bg", "dark-home-bg");
-          } else {
-            document.body.classList.remove(...document.body.classList);
-            document.body.classList.add("home-bg");
-          }
-        }}
-      />
-      <div className="text dark-text">Dark Mode</div>
-      <ToggleSwitch></ToggleSwitch>
-    </div>
+    <>
+      {/* <ToggleSwitch></ToggleSwitch> */}
+      <div style={{ width: '100px', aspectRatio: '1' }}>
+        <ThemeButton name="test" sizeyo="10px"></ThemeButton>
+      </div>
+      <div style={{ width: '300px', aspectRatio: '1' }}>
+        <ThemeButton name="test2" sizeyo="30px"></ThemeButton>
+      </div>
+    </>
   );
 }
 
