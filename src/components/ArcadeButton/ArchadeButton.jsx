9aa647260b8de6aa0384fe3576181518145651bf
Yogeshwar B. <byogeshwarreddy@gmail.com>
2024-04-23 00:49:31 +0530

    Arcade Button in progress

--- src/components/ArcadeButton/ArchadeButton.jsx
+++ src/components/ArcadeButton/ArchadeButton.jsx
@@ -1,5 +1,18 @@
+import '../ArcadeButton/ArcadeButton.css'
+
+const BtnId = 'Test'
+const BtnLabel = 'X'
+
 const ArcadeButton = () => {
-  return <>Arcade Button</>
+  return (
+    <div className='root-div'>
+      <label htmlFor={BtnId} className='btn-box'>
+        <input type='checkbox' id={BtnId} className='checkbox-hide' />
+        <span></span>
+        <div className='icon-div'>{BtnLabel}</div>
+      </label>
+    </div>
+  )
 }
 
 export default ArcadeButton
