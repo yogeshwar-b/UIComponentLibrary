ead5f32a526266ea15a67a7871f6d54b81b66eb3
yogeshwar-b <byogeshwarreddy@gmail.com>
2024-04-23 18:11:20 +0530

    Trashing the previous idea for a new idea.

---
+++ src/components/ArcadeButton/ArcadeButton.jsx
@@ -0,0 +1,10 @@
+import '../ArcadeButton/ArcadeButton.css'
+
+const BtnId = 'Test'
+const BtnLabel = 'X'
+
+const ArcadeButton = () => {
+  return <div>ArcadeButton</div>
+}
+
+export default ArcadeButton
