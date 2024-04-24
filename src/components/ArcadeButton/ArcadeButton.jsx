88445b9c16033b86da182a0f9a113b354dba4fa3
yogeshwar-b <byogeshwarreddy@gmail.com>
2024-04-24 10:26:31 +0530

    Button Animations added.

--- src/components/ArcadeButton/ArcadeButton.jsx
+++ src/components/ArcadeButton/ArcadeButton.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import '../ArcadeButton/ArcadeButton.css'
 
 const BtnId = 'Test'
 const BtnLabel = 'X'
 
 const ArcadeButton = () => {
+  const [btnstate, changebtnstate] = useState('default') // default,pressed,released
+  var cssclass = 'btn-fg btn-fg-color-default'
   return (
-    <button className='btn-bg'>
-      <span className='btn-fg'> Click me</span>
+    <button
+      className='btn-bg'
+      onClick={() => {
+        if (btnstate == 'default') {
+          changebtnstate('pressed')
+        } else {
+          changebtnstate('default')
+        }
+      }}
+    >
+      <span
+        className={
+          cssclass +
+          ' ' +
+          (btnstate == 'pressed' ? 'btn-fg-pos-pressed' : 'btn-fg-pos-default')
+        }
+      >
+        Click me
+      </span>
     </button>
   )
 }
